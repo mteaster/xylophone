44daa245cbd93de83e60c68838bc4843625700d4
mteaster <mteaster@uwm.edu>
2013-10-14 21:39:35 -0500

    initial commit

---
+++ xylophone/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace xylophone
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ xylophone/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="xylophone.MvcApplication" Language="C#" %>
